92388b6dac214ce61c83ef75bbd5a8449f18319f
arslan15 <asarslansaeed1@gmail.com>
2026-09-26 21:35:53 +0500

    fixing of mailing service

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -13,6 +13,7 @@ router.get('/', (req, res) => {
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
   port: process.env.EMAIL_PORT || 587,
+  family: 4,
   secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
